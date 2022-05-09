005dc201d8aac9c7ca647df65bfdc9ee4517792f
Autopopcornshooter <firetruck433@gmail.com>
2022-05-10 00:48:02 +0900

    투사체 변경 -> 불덩이

--- Assets/Scripts/Skills/SkillShoot.cs
+++ Assets/Scripts/Skills/SkillShoot.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a70594cbca0878e84a5ad0ae42e7fd4e716384bb637e1a945f8fa89dfc73c85b
-size 1236
+oid sha256:ffb78cec5303c967854eb0d59b1f8f1cf5e210adcddcb77543c3e81cbaaf2a14
+size 1296
